27499272c0c52031d3463910a58e1ae59cc790ba
justintuition <jihousai@163.com>
2018-04-15 23:08:15 +0800

    First commit

---
+++ Git/src/com/test/Test.java
@@ -0,0 +1,17 @@
+/**
+ * @author jihousai
+ * @date 2018年4月15日 
+ * <p>Description 
+ */
+package com.test;
+
+/**
+* 
+*/
+public class Test
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
